e6f952507b0a4bc39c74442e6fede30f372ae564
Usman <musman@gomarkho.com>
2023-05-04 12:13:32 +0500

    function in python

--- app.py
+++ app.py
@@ -7,18 +7,22 @@
 # print ("I love "+celebrity)
 #
 
-lucky_numbers=[2,5,9,10,4]
-friends=["usman","zoraiz","gomarkho","solarforschools"]
-friends.extend(lucky_numbers) # add to list together
-# append other item to list
-friends.append("Google")
-#insert on specific index
-friends.insert(2,"Good")
-#Remove value form list
-friends.remove("Good")
+# lucky_numbers=[2,5,9,10,4]
+# friends=["usman","zoraiz","gomarkho","solarforschools"]
+# friends.extend(lucky_numbers) # add to list together
+# # append other item to list
+# friends.append("Google")
+# #insert on specific index
+# friends.insert(2,"Good")
+# #Remove value form list
+# friends.remove("Good")
+#
+# #find in list
+# print(friends.index("Google"))
+# friends.clear()
+# print(friends)
 
-#find in list
-print(friends.index("Google"))
-friends.clear()
-print(friends)
-
+# functions
+def hello_world(a, b):
+    return a+b
+print (hello_world(1,2))
