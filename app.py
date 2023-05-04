4920c4b7fba15b7046fe48c91aee4b2221c618a9
Usman <musman@gomarkho.com>
2023-05-04 09:42:41 +0500

    List with basic function

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-color = input("Enter a color")
-plural_noun = input("Enter a plural noun")
-celebrity = input("Enter a celebrity")
+# color = input("Enter a color")
+# plural_noun = input("Enter a plural noun")
+# celebrity = input("Enter a celebrity")
+#
+# print("Roses are "+color)
+# print (plural_noun+ " are blue")
+# print ("I love "+celebrity)
+#
 
-print("Roses are "+color)
-print (plural_noun+ " are blue")
-print ("I love "+celebrity)
+lucky_numbers=[2,5,9,10,4]
+friends=["usman","zoraiz","gomarkho","solarforschools"]
+friends.extend(lucky_numbers) # add to list together
+# append other item to list
+friends.append("Google")
+#insert on specific index
+friends.insert(2,"Good")
+#Remove value form list
+friends.remove("Good")
 
+#find in list
+print(friends.index("Google"))
+friends.clear()
+print(friends)
 
