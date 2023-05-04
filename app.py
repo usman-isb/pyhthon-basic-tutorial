3f238651379e3fd2025ce01d94df88f4d017a40b
Usman <musman@gomarkho.com>
2023-05-04 09:34:00 +0500

    Mad Libs Game (basic variable input from user)

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-name = input("Enter your name: ")
-print("Hello "+ name)
+color = input("Enter a color")
+plural_noun = input("Enter a plural noun")
+celebrity = input("Enter a celebrity")
+
+print("Roses are "+color)
+print (plural_noun+ " are blue")
+print ("I love "+celebrity)
+
+
